78b4bc1cf6e3c20538cb752c998a15d1e0e5edf0
Rimi <lora3226@inu.ac.kr>
2024-09-11 20:18:30 +0900

    [Add] hyolim.py

--- BruteForce/BOJ1342/hyolim.py
+++ BruteForce/BOJ1342/hyolim.py
@@ -2,25 +2,39 @@
 # 입력 받은 문자열을 하나씩 다 뗀 후에 
 # 순열로 엮어서
 # 조건에 맞는지 확인하기
-from itertools import permutations
 
-str=input()
+# 2. 재귀 구현해서 같은 문자가 발생할 경우 중간에 가지치기
 
-answer=set();
-for i in permutations(str):
-	isOk=True;
-	for j in range(len(i)-1):
-		# 만약 같은게 하나라도 있을 경우 패스..
-		if(i[j]==i[j+1]):
-			isOk=False;
-		
-		# print(j,end='')
+str=input();
+ans=set();
+visited=[False for _ in range(len(str))]
 
-	if(isOk):
-		answer.add(i)
-	# print()
+def permutation(depth,curStr):
 
-# for i in answer:
+	# 가지치기
+	if(len(curStr)>1):
+		if(curStr[-1]==curStr[-2]):
+			return;
+
+	if(depth==len(str)):
+		# print(curStr)
+		ans.add(curStr);
+		return;
+
+
+	for i in range(0,len(str)):
+		if not visited[i]:
+			visited[i]=True
+			permutation(depth+1,curStr+str[i])
+			visited[i]=False
+
+permutation(0,'')
+
+# print()
+# for i in ans:
 # 	print(i)
-	
-print(len(answer))
+
+print(len(ans))
+
+
+

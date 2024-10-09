31b67af338adc55db266c5c56ad7c526530d67e7
Eunjin Kim <jennyeunjin@khu.ac.kr>
2024-10-09 20:28:04 +0900

    [Add] eunjin.py

--- DFS & BFS/BOJ11724/eunjin.py
+++ DFS & BFS/BOJ11724/eunjin.py
@@ -1,46 +1,37 @@
 import sys
 sys.setrecursionlimit(100000)
+input = sys.stdin.readline
 
 # 입력 받기
 N, M = map(int, input().split())
 
-matrix = [[0 for _ in range(N+1)] for _ in range(N+1)]
+matrix = [[] for _ in range(N+1)]
 
 for _ in range(M):
     y, x = map(int, input().split())
-    matrix[y][x] = 1
+    matrix[y].append(x)
+    matrix[x].append(y)
 
-print(matrix)
-
-for col in matrix:
-    for row in col:
-        print(row, end=" ")
-    print()
-
-dy = [-1, 0, 1, 0]
-dx = [0, 1, 0, -1]
-
-visited = [[False]*(N+1)for _ in range(N+1)]
+# print(matrix)
 
 
-def dfs(y, x):
-    if visited[y][x]:
-        return
+visited = [False]*(N+1)
 
-    visited[y][x] = True
 
-    for i in range(4):
-        ny = y+dy[i]
-        nx = x + dx[i]
-        if(1 <= ny <= N and 1 <= nx <= N) and not visited[ny][nx]:
-            dfs(ny, nx)
+def dfs(x):
+    global visited, matrix
+    visited[x] = True
+
+    for i in matrix[x]:
+        if not visited[i]:
+            dfs(i)
 
 
 cnt = 0
-for y in range(1, N+1):
-    for x in range(1, N+1):
-        if not visited[y][x] and matrix[y][x] == 1:
-            cnt += 1
-            dfs(y, x)
+for x in range(1, N+1):
+    if not visited[x]:
+        dfs(x)
+        cnt += 1
+
 
 print(cnt)

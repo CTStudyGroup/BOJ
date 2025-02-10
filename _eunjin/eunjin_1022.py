4b494b6ef948593bdfceb6ece5a6fe29e5dbdab7
Eunjin Kim <jennyeunjin@khu.ac.kr>
2025-02-10 16:25:23 +0900

    [Add] eunjin_1022.py

--- _eunjin/eunjin_1022.py
+++ _eunjin/eunjin_1022.py
@@ -1,44 +1,44 @@
 r1, c1, r2, c2 = map(int, input().split())
-K = 5000
-N = K*2+1
-matrix = [[0 for _ in range(N)] for _ in range(N)]
+
+N = max(abs(r1), abs(r2), abs(c1), abs(c2)) * 2 + 1
+
+rows = r2 - r1 + 1
+cols = c2 - c1 + 1
+matrix = [[0] * cols for _ in range(rows)]
 
 # d 0:상, 1:좌, 2:하, 3:우
-dd = [1, 2, 3, 0]
+dr = [-1, 0, 1, 0]
+dc = [0, -1, 0, 1]
 
-r = N//2
-c = N//2
+r, c = N // 2, N // 2
 d = 3
 num = 1
 cnt = 1
-curr_cnt = -1
+curr_cnt = 0
 
-while True:
-    matrix[r][c] = num
-    curr_cnt += 1
+while num <= (N * N):
+    # 좌표가 범위 내에 있을 경우에만 저장
+    if r1 <= r - (N // 2) <= r2 and c1 <= c - (N // 2) <= c2:
+        matrix[r - (N // 2) - r1][c - (N // 2) - c1] = num
+
     num += 1
+    curr_cnt += 1
+    r, c = r + dr[d], c + dc[d]
     # print("r:", r, ",c:", c, ",curr_cnt:", curr_cnt, ", cnt:", cnt, ",d:", d)
-    if curr_cnt == cnt:  # 방향 전환
-        d = dd[d]
+
+    # 방향 전환
+    if curr_cnt == cnt:
+        d = (d + 1) % 4
         curr_cnt = 0
         if d == 1 or d == 3:
             cnt += 1
 
-    if d == 0:
-        r, c = r-1, c
-    elif d == 1:
-        r, c = r, c-1
-    elif d == 2:
-        r, c = r+1, c
-    elif d == 3:
-        r, c = r, c+1
-
     if r < 0 or r >= N or c < 0 or c >= N:
         break
 
-num_len = len(str(num-1))
 
-for r in range(r1+K, r2+K+1):
-    for c in range(c1+K, c2+K+1):
-        print("{:>{}}".format(matrix[r][c], num_len), end=" ")
-    print()
+max_len = max(len(str(matrix[i][j])) for i in range(rows) for j in range(cols))
+
+# 결과 출력
+for row in matrix:
+    print(" ".join(str(x).rjust(max_len) for x in row))

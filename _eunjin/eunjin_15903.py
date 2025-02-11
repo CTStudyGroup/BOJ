9f226cf5a94bf715e10ee566253b37772bc8345b
Eunjin Kim <jennyeunjin@khu.ac.kr>
2025-02-12 01:12:54 +0900

    [Add] eunjin_15903_우선순위큐.py

--- _eunjin/eunjin_15903.py
+++ _eunjin/eunjin_15903.py
@@ -1,10 +1,20 @@
+import heapq
+
 N, M = map(int, input().split())
-cards = list(map(int, input().split()))
+card_list = list(map(int, input().split()))
+
+# heap 생성
+cards = []
+
+for card in card_list:
+    heapq.heappush(cards, card)
 
 for _ in range(M):
-    cards.sort()
-    score = cards[0]+cards[1]
-    cards[0] = score
-    cards[1] = score
+    card1 = heapq.heappop(cards)
+    card2 = heapq.heappop(cards)
+
+    heapq.heappush(cards, card1+card2)
+    heapq.heappush(cards, card1+card2)
+
 
 print(sum(cards))

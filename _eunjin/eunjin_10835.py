a2449d17b02b0b371938ee105abcf3a99c8b3d03
Eunjin Kim <jennyeunjin@khu.ac.kr>
2025-02-11 21:41:07 +0900

    [Add] eunjin_10835.py

--- _eunjin/eunjin_10835.py
+++ _eunjin/eunjin_10835.py
@@ -2,41 +2,19 @@ from collections import deque
 N = int(input())
 left = list(map(int, input().split()))
 right = list(map(int, input().split()))
-A = len(left)
-B = len(right)
 
-# dp[a][b]: 왼쪽 더미에서 a개, 오른쪽 더미에서 b개를 버렸을 때의 최대 점수
-dp = [[0]*(B+1) for _ in range(A+1)]
-
-# 왼쪽 더미 0개, 오른쪽 더미 n개 버림 초기화
-right_q = deque(right[::-1])
-idx = 1
-while right_q and left[0] > right_q[-1]:
-    n = right_q.pop()
-    dp[0][idx] = n
-    idx += 1
+# dp[a][b]: 왼쪽 더미의 카드가 a번째, 오른쪽 더미의 카드가 b번째 카드일 때 최대 점수
+dp = [[0]*(N+1) for _ in range(N+1)]
 
 # dp = 오른쪽 1개 버림, 왼쪽 1개 버림, 왼+오른쪽 1개씩 버림 -> 오른쪽 < 왼쪽인 경우
-# dp[a][b] = max(dp[a][b-1]+right, dp[a-1][b], dp[a-1][b-1])
 # dp = 왼쪽 1개 버림, 왼+오른쪽 1개씩 버림 -> 오른쪽 >= 왼쪽인 경우
-# dp 값이 -1이면 해당 버리기 불가
-left = [0] + left
-right = [0] + right
+# dp[a][b] = max(dp[a][b+1]+right, dp[a+1][b], dp[a+1][b+1])
 
-for l in range(1, A+1):
-    for r in range(1, B+1):
-        # print("l:", l, ", r:", r, ", :", left[l], right[r])
+for l in range(N-1, -1, -1):
+    for r in range(N-1, -1, -1):
         if left[l] > right[r]:
-            dp[l][r] = max(dp[l][r-1]+right[r], dp[l-1][r], dp[l-1][r-1])
-            # print("left > right, dp[", l, "][", r, "]:", dp[l][r])
+            dp[l][r] = max(dp[l][r+1]+right[r], dp[l+1][r], dp[l+1][r+1])
         else:
-            dp[l][r] = max(dp[l-1][r], dp[l-1][r-1])
-            # print("left <= right, dp[", l, "][", r, "]:", dp[l][r])
+            dp[l][r] = max(dp[l+1][r], dp[l+1][r+1])
 
-
-# for row in dp:
-#     for col in row:
-#         print(col, end=" ")
-#     print()
-
-print(dp[A][B])
+print(dp[0][0])

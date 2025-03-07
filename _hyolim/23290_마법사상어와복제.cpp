5510f181c5054a561b3bcfe9b0021a2e8909da4b
Rimi <lora3226@inu.ac.kr>
2025-03-07 21:25:15 +0900

    Update 23290_마법사상어와복제.cpp

--- _hyolim/23290_마법사상어와복제.cpp
+++ _hyolim/23290_마법사상어와복제.cpp
@@ -1,215 +1,150 @@
+
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
-
-/*
-구현해야하는 것
-4. 상어가 이동할 방향 재귀로 찾기
-*/
-
-int m,s;
-// 반시계방향은 -
-string fishDP[8]={"←","↖","↑", "↗", "→", "↘", "↓", "↙"};
-int fishD[8][2]={{0,-1},{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1}}; //물고기 이동 방향
-// 상 좌 하 우
-int sharkD[4][2]={{-1,0},{0,-1},{1,0},{0,1}};
-int fishScent[4][4]={0,};
-vector<int> fish[4][4];
-vector<int> tempfish[4][4];
-
-
-// 상어
-int sharky,sharkx;
-bool visited[4][4];
-vector<int> sharkM;
-int maxeatcnt=0;
-
-void setV(){
-	for(int i=0;i<4;i++){
-		for(int j=0;j<4;j++){
-			visited[i][j]=0;
-		}
-	}
+#define MAX 5
+ 
+int M, S;
+int shark_y, shark_x;
+int dy[] = { 0,-1,-1,-1,0,1,1,1 };
+int dx[] = { -1,-1,0,1,1,1,0,-1 };
+int s_dy[] = { 0,-1,0,1,0 };
+int s_dx[] = { 0,0,-1,0,1 };
+int smell[MAX][MAX];
+vector<int> fish[MAX][MAX];
+vector<int> copy_fish[MAX][MAX];
+vector<int> shark_move;
+int max_eat;
+ 
+ 
+void copy_map(vector<int> a[][MAX], vector<int> b[][MAX]) {
+    for (int i = 1; i < MAX; i++) {
+        for (int j = 1; j < MAX; j++) {
+            b[i][j] = a[i][j];
+        }
+    }
 }
-
-void printFish(){
-	cout<<"---------------\n";
-	for(int i=0;i<4;i++){
-		for(int j=0;j<4;j++){
-			if(fish[i][j].empty()) continue;
-			cout<<i<<" "<<j<<": ";
-			for(auto e:fish[i][j]){
-				cout<<fishDP[e]<<",";
-			}
-			cout<<"\n";
-		}
-	}
+ 
+void move_fish(int time) {
+    vector<int> new_fish[MAX][MAX];
+	// 이차원 배열 탐색, 현재 물고기 있으면 물고기마다 위치 이동 및 이동 방향 재설정
+    for (int i = 1; i < MAX; i++) {
+        for (int j = 1; j < MAX; j++) {
+            for (int k = 0; k < fish[i][j].size(); k++) {
+                int y = i;
+                int x = j;
+                int dir = fish[i][j][k];
+                      
+                int ndir, ny, nx;
+                bool flag = false;
+                for (int i = 0; i < 8; i++) {
+                    ndir = (8 + dir - i) % 8;
+                    ny = y + dy[ndir];
+                    nx = x + dx[ndir];
+                    if (ny < 1 || nx < 1 || ny >= MAX || nx >= MAX)continue;
+                    if (ny == shark_y && nx == shark_x)continue;
+                    if (smell[ny][nx] != 0 && time-smell[ny][nx] <= 2)continue;
+                    flag = true;
+                    new_fish[ny][nx].push_back(ndir);
+                    break;
+                }
+                if (!flag) {
+                    new_fish[y][x].push_back(dir);
+                }
+            }
+        }
+    }
+    copy_map(new_fish, fish);
 }
-
-void input(){
-	cin>>m>>s;
-	for(int i=0;i<m;i++){
-		int y,x,d;
-		cin>>y>>x>>d;
-		fish[y-1][x-1].push_back(d-1); 
-	}
-	cin>>sharky>>sharkx;
-	sharky--;
-	sharkx--;
+ 
+int shark_simulation(vector<int> route) {
+    bool visit[MAX][MAX] = { false, };
+    int res = 0;
+    int s_y = shark_y;
+    int s_x = shark_x;
+    for (auto dir : route) {
+        s_y += s_dy[dir];
+        s_x += s_dx[dir];
+        if (s_y < 1 || s_x < 1 || s_y >= MAX || s_x >= MAX)return -1;
+        if(!visit[s_y][s_x]){
+            visit[s_y][s_x] = true;
+            res += fish[s_y][s_x].size();
+        }
+    }
+    return res;
 }
-
-void setTempFish(){
-	for(int i=0;i<4;i++){
-		for(int j=0;j<4;j++){
-			tempfish[i][j].clear();
-		}
-	}
+// 상어 이동 시뮬레이션, 어디로 가는 것이 물고기를 최대로 먹을지
+void search_best_move(vector<int> route) {
+    if (route.size() == 3) {
+    	// 3번 이동 했다면 물고기를 몇 마리 먹을 수 있는지 확인
+        int eat = shark_simulation(route);
+        if (eat > max_eat) {
+            max_eat = eat;
+            shark_move = route;
+        }
+        return;
+    }
+    for (int i = 1; i <= 4; i++) {
+        route.push_back(i);
+        search_best_move(route);
+        route.pop_back();
+    }
 }
-
-void printTempFish(){
-	cout<<"-------temp--------\n";
-	for(int i=0;i<4;i++){
-		for(int j=0;j<4;j++){
-			if(tempfish[i][j].empty()) continue;
-			cout<<i<<" "<<j<<": ";
-			for(auto e:tempfish[i][j]){
-				cout<<fishDP[e]<<",";
-			}
-			cout<<"\n";
-		}
-	}
+ 
+// 이동방향이 결정되었다면 상어 이동, 이동 중 물고기를 먹었다면 해당 위치에 냄새를 남김
+void move_shark(int time) {
+    for (auto dir : shark_move) {
+        shark_y += s_dy[dir];
+        shark_x += s_dx[dir];
+        if (fish[shark_y][shark_x].size() != 0) {
+            fish[shark_y][shark_x].clear();
+            smell[shark_y][shark_x] = time;
+        }
+    }
 }
-
-void fishMove(){
-	int nexty,nextx;
-	setTempFish();
-	for(int i=0;i<4;i++){
-		for(int j=0;j<4;j++){
-			for(auto e:fish[i][j]){
-				int nextd=e;
-				for(int z=0;z<8;z++){
-					nextd=(8+e-z)%8;
-
-					nexty=fishD[nextd][0]+i;
-					nextx=fishD[nextd][1]+j;
-
-					// 범위 조건 만족하고
-					if(nexty<0||nextx<0||nexty>3||nextx>3) continue;
-
-					// 상어
-					if(nexty==sharky&&nextx==sharkx) continue;
-
-					// 물고기 냄새
-					if(fishScent[nexty][nextx]>0) continue;
-
-					tempfish[nexty][nextx].push_back(nextd);
-					break;
-				}
-			}
-		}
-	}
+ 
+void paste_fish() {
+    for (int i = 1; i < MAX; i++) {
+        for (int j = 1; j < MAX; j++) {
+            for(auto dir:copy_fish[i][j]){
+                fish[i][j].push_back(dir);
+            }
+        }
+    }
 }
-
-void findsharkMove(vector<int> s,int eatcnt,int y,int x){
-	if(s.size()>=3){
-		if(maxeatcnt<eatcnt){
-			cout<<maxeatcnt;
-			maxeatcnt=eatcnt;
-			sharkM=s;
-		}
-		return;
-	}
-
-	for(int i=0;i<4;i++){
-		int nexty=y+sharkD[i][0];
-		int nextx=x+sharkD[i][1];
-
-		// 범위 조건 만족하고
-		if(nexty<0||nextx<0||nexty>3||nextx>3) continue;
-
-		// 방문한 적 없는
-		// if(visited[nexty][nextx]) continue;
-
-		// visited[nexty][nextx]=true;
-		s.push_back(i);
-		findsharkMove(s,eatcnt+tempfish[nexty][nextx].size(),nexty,nextx);
-		// visited[nexty][nextx]=false;
-		s.pop_back();
-	}
-}
-
-void copyFish(){
-	for(int i=0;i<4;i++){
-		for(int j=0;j<4;j++){
-			for(auto e:tempfish[i][j]){
-				fish[i][j].push_back(e);
-			}
-		}
-	}
-}
-
-void sharkMove(){
-	// 상어가 갈 방향 찾기
-	setV();
-	vector<int> s;
-	sharkM.clear();
-	printTempFish();
-	findsharkMove(s,0,sharky,sharkx);
-	for(auto e:sharkM){
-		cout<<e<<" ";
-	}
-	cout<<"\n";
-	for(auto e:sharkM){
-		sharky+=sharkD[e][0];
-		sharkx+=sharkD[e][1];
-
-		// 만약 물고기가 있을 경우
-		if(!tempfish[sharky][sharkx].empty()){
-			fishScent[sharky][sharkx]=;
-			tempfish[sharky][sharkx].clear();
-		}
-	}
-}
-
-void minusScent(){
-	for(int i=0;i<4;i++){
-		for(int j=0;j<4;j++){
-			if(fishScent[i][j]) fishScent[i][j]-=1;
-		}
-	}
-}
-
-void solve(){
-	while(s--){
-		// 물고기 흔적 냄새 지우기
-		minusScent();
-
-		// 물고기 이동
-		fishMove();
-
-		// 상어 이동
-		sharkMove();
-
-		// 물고기 복ㅈㅔ
-		copyFish();
-
-		printFish();
-	}
-}
-
-void output(){
-	int answer=0;
-	for(int i=0;i<4;i++){
-		for(int j=0;j<4;j++){
-			answer+=fish[i][j].size();
-		}
-	}
-	cout<<answer;
-}
-
-int main(){
-	input();
-	solve();
-	output();
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+	cin.tie(0);
+    
+    cin >> M >> S;
+    int y, x, dir;
+    for (int i = 1; i <= M; i++) {
+        cin >> y >> x >> dir;
+        fish[y][x].push_back(dir-1);
+    }
+    cin >> shark_y >> shark_x;
+    
+    for (int i = 1; i <= S; i++) {
+    	//현재 물고기 위치 저장
+        copy_map(fish, copy_fish);
+        move_fish(i);
+        // 상어 이동 시뮬레이션을 위한 설정, 상어 이동
+        max_eat = -1;
+        vector<int> temp;
+        search_best_move(temp);
+        move_shark(i);
+        // 저장된 물고기 위치 복제
+        paste_fish();
+    }
+    
+	// 현재 남아 있는 물고기 수 
+    int res = 0;
+    for (int i = 1; i < MAX; i++) {
+        for (int j = 1; j < MAX; j++) {
+            res += fish[i][j].size();
+        }
+    }
+    cout << res;
+    return 0;
 }

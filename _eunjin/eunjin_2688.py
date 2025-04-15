0ae239e5563be261aa523d9cb6055d035b16324d
Eunjin Kim <jennyeunjin@khu.ac.kr>
2025-04-15 12:29:02 +0900

    Add eunjin_2688.py
    정답 풀이

--- _eunjin/eunjin_2688.py
+++ _eunjin/eunjin_2688.py
@@ -1,21 +1,20 @@
-# 시간 초과 풀이
 import sys
 input = sys.stdin.readline
 
 T = int(input())
 
-def dfs(n, depth):
-    global answer
-    if depth == N:
-        answer += 1
-        return
-
-    for i in range(n, 10):
-        dfs(i, depth + 1)
-
-
+# dp
+# dp[a][b]: a자리의 마지막 숫자가 b일 때 가능한 답의 개수
+# dp[y][x] = sum(dp[y-1][:x+1])
 for _ in range(T):
     N = int(input())
-    answer = 0
-    dfs(0, 0)
-    print(answer)
+    if N == 1:
+        print(10)
+        continue
+    dp = [[0] * 10 for _ in range(N + 1)]
+    dp[1] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+    for y in range(2, N + 1):
+        for x in range(10):
+            dp[y][x] = sum(dp[y - 1][:x + 1])
+    print(sum(dp[N]))

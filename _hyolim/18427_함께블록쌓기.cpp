2be47c48594c69bb9550ebaf1b6221dbc074a8c7
Rimi <lora3226@inu.ac.kr>
2025-05-22 09:33:01 +0900

    Update 18427_함께블록쌓기.cpp

--- _hyolim/18427_함께블록쌓기.cpp
+++ _hyolim/18427_함께블록쌓기.cpp
@@ -1,70 +1,35 @@
 #include <iostream>
+#include <sstream>
 #include <vector>
-#include <string>
-#include <algorithm>
-#define DIV 10007
 
 using namespace std;
 
-int n,m,h;
+int dp[51][1001];
 vector<int> block[51];
-long dp[51][1001]={0,};
-
-void input(){
-	cin>>n>>m>>h;
-
-	string temp;
-	getline(cin,temp);
-
-	for(int i=1;i<=n;i++){
-		string temp;
-		getline(cin,temp);
-
-		string tt;
-		for(auto e:temp){
-			if(e!=' '){
-				tt+=e;
-			}else{
-				block[i].push_back(stoi(tt));
-				tt="";
-			}
-		}
-		block[i].push_back(stoi(tt));
-	}
-
-	// for(int i=1;i<=n;i++){
-	// 	for(auto e:block[i]){
-	// 		cout<<e<<" ";
-	// 	}
-	// 	cout<<"\n";
-	// }
-}
-
-void solve(){
-	// dp[0][0]=1;
-	for(int i=1;i<=n;i++){
-		// dp[i][j] = 각 사람마다 j까지 올릴 수 있는 방법의 수
-		for(auto e:block[i]){
-			for(int j=1;j<=h;j++){
-				if(j<e) dp[i][j]=(dp[i][j]+dp[i-1][j])%DIV;
-				else{
-					dp[i][j]=(dp[i][j]+dp[i-1][j-e]+1)%DIV;
-				}
-				cout<<i<<" "<<j<<" "<<e<<" "<<dp[i][j]<<"\n";
-
-			}
-		}
-	}
-	cout<<"======\n";
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=h;j++){
-			cout<<dp[i][j]<<" ";
-		}
-		cout<<"\n";
-	}
-}
 
 int main(){
-	input();
-	solve();
+	cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+    int N,M,H;
+    cin >> N >> M >> H;
+    cin.ignore();
+    for(int i=1;i<=N;i++){
+    	string str;
+    	getline(cin,str);
+    	stringstream sstr(str);
+    	int num;
+
+    	while(sstr>>num) block[i].push_back(num);
+    }
+
+    for(int i=0;i<=N;i++) dp[i][0]=1;
+
+    for(int i=1;i<=N;i++){
+    	for(int j=1;j<=H;j++){
+    		for(int k=0;k<block[i].size();k++){
+    			if(j>=block[i][k]) dp[i][j]=(dp[i][j]+dp[i-1][j-block[i][k]])%10007;
+    		}
+    		dp[i][j]=(dp[i][j]+dp[i-1][j])%10007;
+    	}
+    }
+    cout<<dp[N][H];
 }

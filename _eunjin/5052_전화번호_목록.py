7eb8004d38ab45c038fa91e196e0aec71750e616
Eunjin Kim <jennyeunjin@khu.ac.kr>
2025-06-19 07:33:45 +0900

    5052_전화번호_목록.py
    트라이

---
+++ _eunjin/5052_전화번호_목록.py
@@ -0,0 +1,89 @@
+import sys
+input = sys.stdin.readline
+
+# 트라이
+# arr 내림차순 정렬한 뒤에 앞에서부터 트라이에 노드 추가
+
+class Node(object):
+    def __init__(self, key, data=None):
+        self.key = key  # 값으로 입력될 문자
+        self.data = data  # 문자열 종료 여부 flag
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.head = Node(None)
+
+    def insert(self, string):
+        current_node = self.head
+
+        for char in string:
+            if char not in current_node.children:
+                current_node.children[char] = Node(char)
+            current_node = current_node.children[char]
+        current_node.data = string
+
+    def search(self, string):
+        current_node = self.head
+
+        for char in string:
+            if char in current_node.children:
+                current_node = current_node.children[char]
+            else:
+                return False
+
+        if current_node.data:
+            return True
+        else:
+            return False
+
+    def starts_with(self, prefix):
+        current_node = self.head
+        words = []
+
+        for p in prefix:
+            if p in current_node.children:
+                current_node = current_node.children[p]
+            else:
+                return None
+
+        current_node = [current_node]
+        next_node = []
+
+        while True:
+            for node in current_node:
+                if node.data:
+                    words.append(node.data)
+                next_node.extend(list(node.children.values()))
+            if len(next_node):
+                current_node = next_node
+                next_node = []
+            else:
+                break
+
+        return words
+
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    arr = []
+    trie = Trie()
+    answer = "YES"
+
+    for _ in range(N):
+        num = input().strip()
+        arr.append(num)
+
+    arr.sort(reverse=True)
+
+    for word in arr:
+        trie.insert(word)
+
+        startWith = trie.starts_with(word)
+
+        if len(startWith) > 1:
+            answer = "NO"
+            break
+
+    print(answer)

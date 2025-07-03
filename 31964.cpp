028a759ca075e681400c1be1f9c69d08ba9de2cf
KII1ua <sung25480@naver.com>
2025-07-04 08:41:56 +0900

    31964.cpp add

---
+++ 31964.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define MAX 1e9
+
+struct coordinate {
+    int x;
+    int y;
+};
+
+int dx[] = {-1, 0, 0, 1, 1, -1, -1, 1};
+int dy[] = {0, -1, 1, 0, -1, 1, -1, 1};
+//int dx[] = {0, -1, -1, 0, 1, 1, 1, 0, -1};
+//int dy[] = {0,0, -1, -1, -1, 0, 1, 1, 1};
+//int dx[] = {-2, -2, -1, -1, 1, 1, 2, 2};
+//int dy[] = {-1, 1, -2, 2, -2, 2, -1, 1};
+int N;
+vector<pii> v;
+bool visited[3001];
+int res;
+
+void solve() {
+    sort(v.begin(), v.end());
+
+    res = max(v[N-1].first, v[N-1].second);
+
+    for(int i = N-1; i >= 1; i--) {
+        int dist = v[i].first - v[i-1].first;
+        res = max(res + dist, v[i-1].second);
+    }
+
+    cout << res + v[0].first;
+}
+
+void input() {
+    cin >> N;
+
+    for(int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+        v.push_back({a, 0});
+    }
+
+    for(int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+        v[i].second = a;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+   input();
+   solve();
+
+//    int row = 3;
+//    int column = 4;
+//
+//    int **arr = (int**)malloc(row * sizeof(int*));
+//
+//    for(int i = 0; i < row; i++) {
+//        arr[i] = (int*)malloc(column * sizeof(int));
+//    }
+//
+//    cout << arr[0][0];
+
+
+//    int **arr = (int **)malloc(row * sizeof(int*));
+//
+//    for(int i = 0; i < row; i++) {
+//        arr[i] = (int*)malloc(column * sizeof(int));
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        for(int j = 0; j < column; j++) {
+//            arr[i][j] = j;
+//        }
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        for(int j = 0; j < column; j++) {
+//            cout << arr[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//    for(int i = 0; i < row; i++) {
+//        free(arr[i]);
+//    }
+//    free(arr);
+//
+//    cout << sizeof(int);
+//    cout << sizeof(int*);
+
+
+//    int row = 3;
+//
+//    int *arr = (int*)malloc(10000 * sizeof(int));
+//
+//    for(int i = 0; i < 10000; i++) {
+//        arr[i] = i;
+//    }
+//
+//    for(int i = 0; i < 100; i++) {
+//        cout << arr[i] << endl;
+//    }
+//
+//    free(arr);
+
+
+//    int arr[] = {1, 2, 3, 4, 5};
+//    int *ptr = arr;
+//    int **tmp = ptr;
+
+//    struct pointwrapper pwrap;
+//    struct jsu st = {"데이터", 4, 5, 4, 3};
+//
+//    pwrap.ptr = &st;
+//
+//    cout << pwrap.ptr << endl;
+//
+//    cout << pwrap.ptr -> os;
+
+//    struct jsu st[3] = {{"데이터1", 95, 88}, {"데이터2", 84, 91},
+//                        {"데이터3", 86, 75}};
+//
+//    struct jsu* p;
+//
+//    p = &st[0];
+//
+//    (p + 1) -> hab = (p + 1) -> os + (p + 2) -> db;
+//
+//    cout << p;
+//    cout << *p.os;
+}

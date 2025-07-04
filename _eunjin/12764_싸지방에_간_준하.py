a70ec12321be3bcf1b9bf91e71371d991aabfedf
Eunjin Kim <jennyeunjin@khu.ac.kr>
2025-07-04 10:13:57 +0900

    12764_싸지방에_간_준하.py

--- _eunjin/12764_싸지방에_간_준하.py
+++ _eunjin/12764_싸지방에_간_준하.py
@@ -9,38 +9,26 @@ info = [list(map(int, input().split())) for _ in range(N)]
 # 컴퓨터 최소 개수, 각 컴퓨터별 사용한 사람 수
 
 # 시작 시간 순으로 정렬해 앞에서부터 배정
-info.sort(key=lambda x: [x[0], x[1]])
+info.sort(key=lambda x: x[0])
 
-pq = []  # 컴퓨터 우선순위큐 (사용 종료 시간, 컴퓨터 번호, 누적 사용자 수)
+using = []  # 사용중인 컴퓨터 우선순위큐 (사용 종료 시간, 컴퓨터 번호) -> 얘는 사용 종료 시간 기준 최솟값을 뽑는 용도
+empty = []  # 비어있는 컴퓨터 우선순위큐 (컴퓨터 번호) -> 얘는 컴퓨터 번호 기준 최솟값을 뽑는 용도
+computer_users = []  # 컴퓨터별 사용자수
 
-num = 0
 for start, end in info:
-    if pq:  # 컴퓨터가 있으면
-        if pq[0][0] < start:  # 앞 사용자가 다 쓰고 나간 경우
-            # 비어있는 컴퓨터 모두 찾기
-            empty_computers = []
-            while pq and pq[0][0] < start:
-                empty_computers.append(heapq.heappop(pq))
+    # 현재 시간 기준 using 큐에서 사용 가능한 컴퓨터 뽑아서 empty 큐로 옮김
+    while using and using[0][0] <= start:
+        _, num = heapq.heappop(using)
+        heapq.heappush(empty, num)
 
-            empty_computers.sort(key=lambda x: x[1])  # 컴퓨터 번호 순으로 정렬
-            # print("empty_computers:", empty_computers)
+    if empty:  # 비어있는 컴퓨터가 있으면
+        num = heapq.heappop(empty)  # empty큐에서 하나 뽑아서 사용 처리
+        computer_users[num] += 1
+    else:  # 비어있는 컴퓨터 없으면
+        num = len(computer_users)  # 새 컴퓨터 추가
+        computer_users.append(1)
 
-            # 컴퓨터 사용 처리
-            use = empty_computers[0]
-            heapq.heappush(pq, (end, use[1], use[2] + 1))
+    heapq.heappush(using, (end, num))  # 현재 유저가 사용할 컴퓨터 using 큐에 추가
 
-            # 나머지 비어있는 컴퓨터는 그대로 pq에 넣기
-            for i in range(1, len(empty_computers)):
-                heapq.heappush(pq, empty_computers[i])
-        else:  # 새 컴퓨터 추가 필요한 경우
-            num += 1
-            heapq.heappush(pq, (end, num, 1))
-    else:  # 컴퓨터 없으면 새 컴퓨터 추가
-        num += 1
-        heapq.heappush(pq, (end, num, 1))
-
-
-pq.sort(key=lambda x: x[1])
-
-print(len(pq))
-print(' '.join(map(str, list(user for _, _, user in pq))))
+print(len(computer_users))
+print(*computer_users)

56e83116ef4a7ae30983fcadc85f96a4fbaf8d28
지현서 <113419425+3veryDay@users.noreply.github.com>
2025-07-22 09:07:54 +0900

    Update 9184.py

--- _hyunseo/9184.py
+++ _hyunseo/9184.py
@@ -1,9 +1,9 @@
 dp = [[[0] * 21 for _ in range(21)] for _ in range(21)]
 
 def w(a, b, c):
-    if a <= 0 or b <= 0 or c <= 0:
+    if min(a, b, c) <= 0:
         return 1
-    if a > 20 or b > 20 or c > 20:
+    if  max(a,b,c) > 20:
         return w(20, 20, 20)
     if dp[a][b][c] != 0:
         return dp[a][b][c]

fa1c098f3ea72f35bde5fea0bacda1c735ef77f1
Eunjin Kim <jennyeunjin@khu.ac.kr>
2025-08-01 09:57:13 +0900

    16940_BFS_스페셜_저지.py

--- _eunjin/16940_BFS_스페셜_저지.py
+++ _eunjin/16940_BFS_스페셜_저지.py
@@ -9,40 +9,38 @@ for _ in range(N - 1):
     adj_list[a].append(b)
     adj_list[b].append(a)
 
-orders = list(map(int, input().split()))
+order = list(map(int, input().split()))
 
 # 방문하지 않은 정점 큐에 넣을 때 순서 고려하지 않음
 # bfs 방문 경로가 여러개가 나온다.
 # 직전에 어떤 노드를 먼저 큐에 넣었냐에 따라 다음 노드가 달라짐
-# adj_list에서 다음 노드 뽑을 때 주어진 노드가 있으면 그 노드를 큐의 제일 앞에 넣기?
+
+visited = [False] * (N + 1)
+position = [0] * (N + 1)
+
+# 순서 배열에서 각 노드의 순서를 기록
+for i in range(N):
+    position[order[i]] = i
+
+# 각 노드의 인접 리스트를 방문 순서 기준으로 정렬
+for i in range(1, N + 1):
+    adj_list[i].sort(key=lambda x: position[x])
 
 q = deque()
-visited = [False] * (N + 1)
-
-q.append((1, 0))  # node, depth
+q.append(1)
 visited[1] = True
-
-visited_depth = []
+visit_order = []
 
 while q:
-    node, depth = q.popleft()
-    # print("curr node:", node, depth)
-    visited_depth.append(depth)
+    node = q.popleft()
+    visit_order.append(node)
 
     for adj_node in adj_list[node]:
         if not visited[adj_node]:
-            if adj_node == orders[depth + 1]:
-                q.appendleft((adj_node, depth + 1))
-            else:
-                q.append((adj_node, depth + 1))
-
             visited[adj_node] = True
+            q.append(adj_node)
 
-
-# print(visited_depth)
-
-for i in range(1, len(visited_depth)):
-    if visited_depth[i - 1] > visited_depth[i]:
-        print(0)
-        exit()
-print(1)
+if visit_order == order:
+    print(1)
+else:
+    print(0)

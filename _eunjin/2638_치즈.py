df5ceaf5fb4063d9564a8bda33fb3417c9376cdb
Eunjin Kim <jennyeunjin@khu.ac.kr>
2025-08-11 09:08:46 +0900

    2638_치즈.py
    14M

--- _eunjin/2638_치즈.py
+++ _eunjin/2638_치즈.py
@@ -1,50 +1,53 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(N)]
 
-# 외부 영역 기준으로 bfs해서 경계선의 치즈 모두 찾아내기
-# 외부 영역을 어떻게 판별할 수 있는지
-# 해당 영역이 상하좌우로 치즈와 인접해있는지 여부로 판단?
-dy = [1, 0, -1, 0]
-dx = [0, 1, 0, -1]
+# 가장자리에서 bfs 시작, 각 치즈 좌표마다 방문 횟수 구하기
+# 방문 횟수가 2 이상이면 해당 치즈 좌표는 녹는 좌표로 취급
 
-def find():
-    visited = [[False] * M for _ in range(N)]
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
+def bfs():
+    q = deque()
+    q.append((0, 0))  # y,x
+    visited[0][0] = 1
 
+    while q:
+        y, x = q.popleft()
+
+        for i in range(4):
+            ny, nx = y + dy[i], x + dx[i]
+
+            if ny < 0 or ny >= N or nx < 0 or nx >= M:
+                continue
+
+            if matrix[ny][nx] == 0:
+                if not visited[ny][nx]:
+                    q.append((ny, nx))
+                    visited[ny][nx] = 1
+            else:  # 치즈
+                visited[ny][nx] += 1  # 방문 횟수 추가
+
+def remove_cheese():
     for y in range(N):
         for x in range(M):
-            if matrix[y][x] and not visited[y][x]:
-                # bfs
-                temp = []
+            if matrix[y][x] == 1 and visited[y][x] >= 2:
+                matrix[y][x] = 0
 
-                q = deque()
-                visited[y][x] = True
-                q.append((y, x))
-
-                while q:
-                    cy, cx = q.popleft()
-
-                    for i in range(4):
-                        ny, nx = cy + dy[i], cx + dx[i]
-                        if ny < 0 or ny >= N or nx < 0 or nx >= M:
-                            continue
-
-                        if matrix[ny][nx] and not visited[ny][nx]:
-                            visited[ny][nx] = True
-                            q.append((ny, nx))
-
-
-def valid():
+def has_cheese():
+    ret = 0
     for row in matrix:
-        if sum(row) > 0:
-            return False
-    return True
+        ret += sum(row)
+    return ret
 
-T = 0
-# while valid():
-#     # 외부 영역 찾기
-#     # 치즈 녹이기
-#     T += 1
+answer = 0
+while has_cheese():
+    visited = [[0] * M for _ in range(N)]
+    bfs()
+    remove_cheese()
+    answer += 1
 
-print(T)
+print(answer)

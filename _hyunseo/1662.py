eb4592c16e16f0e05d8aa93cc52c377a7b8539a8
지현서 <113419425+3veryDay@users.noreply.github.com>
2025-08-22 07:44:41 +0900

    ⭐️ 250822 : [BOJ 1662] 압축

--- _hyunseo/1662.py
+++ _hyunseo/1662.py
@@ -1,35 +1,32 @@
-# 메모리 초과 풀이
 import sys
 from collections import deque
 
 
 def solve(idx, repeat_num) :
-    d = deque()
-    
+    total = 0
     while input_[idx] != ")" :
         '''idx는 ( 다음부터 시작'''
         if input_[idx+1] == "(" :
-            tmp_str, end = solve(idx+2, input_[idx])
-            d.append(tmp_str)
+            tmp, end = solve(idx+2, input_[idx])
+            total += tmp
             idx = end
         else : 
-            d.append(input_[idx])
+            total += 1
             idx += 1
-    return str(''.join(d))* int(repeat_num), idx + 1
+    return total* int(repeat_num), idx + 1
 
-input_ = input()
-answer = ''
+input_ = input().strip()
+answer = 0
 
 i = 0
 while i < len(input_) -1  :
     if input_[i+1] == '(' :
-        tmp_str, end = solve(i+2, input_[i])
+        tmp, end = solve(i+2, input_[i])
         i = end
-        answer += tmp_str
+        answer += tmp
     else :
-        answer += input_[i]
+        answer += 1
         i += 1
 if input_[-1] != ')' :
-    answer += input_[-1]
+    answer += 1
 print(answer)
-print(len(answer))

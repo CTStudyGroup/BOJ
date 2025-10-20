2272ec83d54c16a07740a461b066cdfb5cba0b73
KII1ua <sung25480@naver.com>
2025-10-21 07:35:01 +0900

    1027.cpp add

---
+++ _sungyoon/1027.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long ll;
+#define endl "\n"
+#define MAX 1e9
+
+struct coordinate {
+    int x;
+    int y;
+    int r;
+};
+
+struct horse {
+    int x;
+    int y;
+    int dir;
+};
+
+// int dx[] = {-1, 1, 0, 0, 1, -1, -1, 1};
+// int dy[] = {0, 0, -1, 1, -1, 1, -1, 1};
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+int N;
+ll dp[36];
+
+void solve() {
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for(int i = 2; i <= 35; i++) {
+        int cnt = i-1;
+        for(int j = 0; j < i; j++) {
+            dp[i] += (dp[j] * dp[cnt--]);
+        }
+    }
+
+    cout << dp[N];
+}
+
+void input() {
+    cin >> N;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    input();
+    solve();
+}
+

e1c9657c4eddabae5ec1250c93f72f60bf45af06
sksn12 <sksn12@naver.com>
2025-11-25 01:18:34 +0900

    25/11/25

---
+++ _youngman/최대정사각형.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            if (N == 0 && M == 0) break;
+
+            int[][] board = new int[N][M];
+            int[][] dp = new int[N][M];
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < M; j++) {
+                    board[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int maxSize = 0;
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+
+                    if (board[i][j] == 1) {
+                        // 첫 행/열은 그대로
+                        if (i == 0 || j == 0) {
+                            dp[i][j] = 1;
+                        } else {
+                            dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                        }
+
+                        maxSize = Math.max(maxSize, dp[i][j]);
+                    }
+                }
+            }
+
+            System.out.println(maxSize);
+        }
+    }
+}

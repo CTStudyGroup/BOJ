4dc46a1da59c4e59529425b3f8452565aa507a17
sunha20 <c.seonha20@gmail.com>
2025-11-25 00:29:15 +0900

    add: 4095 최대 정사각형

---
+++ _sunha/4095_최대 정사각형.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        while (true) {
+            String line = br.readLine();
+
+            st = new StringTokenizer(line);
+
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            // 종료 조건
+            if (N == 0 && M == 0) break;
+
+            int[][] dp = new int[N][M];
+            int maxSide = 0;
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < M; j++) {
+                    int val = Integer.parseInt(st.nextToken());
+
+                    if (val == 1) {
+                        dp[i][j] = 1; // 기본값 1
+
+                        if (i > 0 && j > 0) {
+                            dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+                        }
+
+                        // 최댓값 갱신
+                        maxSide = Math.max(maxSide, dp[i][j]);
+                    }
+                }
+            }
+
+            System.out.println(maxSide);
+        }
+    }
+}
